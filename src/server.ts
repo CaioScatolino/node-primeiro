2d0db8eb066b7814f8bb6d0975913d7886e1eff3
Caio Scatolino <caio.scatolino@outlook.com>
2025-12-05 20:14:47 -0300

    Remove unused files and implement helmet for security in server setup

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,20 @@
 import express from 'express';
+import helmet from  'helmet';
+import path from 'path';
 
 const server = express();
+
+server.use(helmet());
+server.use(express.json());
+server.use(express.urlencoded({extended: true}));
+server.use(express.static(path.join(__dirname, '../public')));
+
+
 
 server.get('/', (req, res) => {
     let name = `Caio`
     let age = 23
-  res.send(`Olá, meu nome é ${name} e eu tenho ${age} anos.`);
+    res.json({ name, age });
 });
 
 server.listen(3000, () => { 
