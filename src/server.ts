fc8501352ffb8e684eb1b7680893c1948c9e49b7
Caio Scatolino <caio.scatolino@outlook.com>
2025-12-05 08:19:07 -0300

    Iniciando Express

--- src/server.ts
+++ src/server.ts
@@ -1,23 +1,13 @@
-import express from "express";
-import helmet from "helmet";
-import path from 'path';
-import router from './routes'
-import { errorHandler, notFoundRequest } from "./routes/errorhandler";
+import express from 'express';
 
+const server = express();
 
-const server = express()
+server.get('/', (req, res) => {
+    let name = `Caio`
+    let age = 23
+  res.send(`Olá, meu nome é ${name} e eu tenho ${age} anos.`);
+});
 
-server.use(helmet()) // Security middleware
-server.use(express.json()) // Middleware to parse JSON requests
-server.use(express.urlencoded({ extended: true })) // Middleware to parse URL-encoded requests
-server.use(express.static(path.join(__dirname, '../public'))) // Middleware to serve static files
-
-
-server.use('/', router) // Middleware to use the routes defined in the router
-server.use(notFoundRequest) // Middleware to handle 404 errors
-server.use(errorHandler) // Middleware to handle 500 errors
-
-
-server.listen(3000, () => {
-    console.log('Servidor is running on http://localhost:3000')
+server.listen(3000, () => { 
+    console.log(`Server is running on http://localhost:3000`);
 })
