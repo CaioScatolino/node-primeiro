6820ae36423944472683a04fdac3bea160cb2aa4
Caio Scatolino <caio.scatolino@outlook.com>
2025-12-08 19:49:02 -0300

    Add error handling middleware for not found and internal server errors

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import helmet from 'helmet';
 import path from 'path';
 import router from './routes';
+import { errorHandler, notFoundRequest } from './routes/errorHandler';
 
 
 const server = express();
@@ -11,7 +12,9 @@ server.use(express.json());
 server.use(express.urlencoded({ extended: true }));
 server.use(express.static(path.join(__dirname, '../public')));
 
-server.use('/', router);
+server.use('/', router); 
+server.use(notFoundRequest); 
+server.use(errorHandler); 
 
 server.listen(3000, () => {
     console.log(`Server is running on http://localhost:3000`);
